c1a30dd190cd4fae004356a5cec1d26500ea72b0
Theodore07 <91310089+Theodore07@users.noreply.github.com>
2024-04-20 22:47:37 +0800

    updated from ROG

---
+++ sa_bpp/problem.java
@@ -0,0 +1,20 @@
+package sa_bpp;
+import java.util.ArrayList;
+import java.util.List;
+
+class problem {
+    
+    private String p_name;
+    private int w;
+    private int c;
+    private List<Integer> ItemW;
+    private List<Integer> ItemC;
+
+    public bpp(String name, int weight, int capacity){
+        this.p_name = name;
+        this.w = w;
+        this.c = c;
+        this.ItemW = new ArrayList<>()
+
+    }
+}
